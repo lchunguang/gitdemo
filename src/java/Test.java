0cba6444c001b00172ecfd0a2b2dade4b1d11f26
lcg <lichunguangs@163.com>
2019-11-27 16:44:21 +0800

    新增文件
    (cherry picked from commit 6c3d66d)

---
+++ src/java/Test.java
@@ -0,0 +1,12 @@
+/**
+ * @Author : lcg
+ * @Date : 2019/11/27
+ * @Descripton :...
+ */
+public class Test
+
+{
+    public static void main(String[] args) {
+        System.out.println("文件");
+    }
+}

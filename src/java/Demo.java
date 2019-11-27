2279113b97aa63bf3f1c805002ddced3aa83f4d8
lcg <lichunguangs@163.com>
2019-11-27 14:26:13 +0800

    初始化项目

---
+++ src/java/Demo.java
@@ -0,0 +1,10 @@
+/**
+ * @Author : lcg
+ * @Date : 2019/11/27
+ * @Descripton :...
+ */
+public class Demo {
+    public static void main(String[] args) {
+        System.out.println("test");
+    }
+}
